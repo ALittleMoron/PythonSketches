2b9453083947d30d0b0b2975f0adc6300bb21596
ALittleMoron <dima.lunev14@gmail.com>
2020-05-31 22:51:09 +0300

    added return and real resize

--- image_resize.py
+++ image_resize.py
@@ -1,4 +1,5 @@
 from PIL import Image
+import math
 
 def img_resize(f_size="", path="", directory=""):
     if not f_size and not path:
@@ -7,16 +8,23 @@ def img_resize(f_size="", path="", directory=""):
     if directory:
         with Image.open(directory+path) as img:
             size = f_size.split('x')
-            img.show()
+            size = tuple([int(i) for i in size])
+            img.thumbnail(size)
+            return img
+
     elif not directory:
         with Image.open(path) as img:
             size = f_size.split('x')
+            size = tuple([int(i) for i in size])
+            img.thumbnail(size)
+            return img
+
     else:
         raise Exception("IDK what went wrong")
 
 
-img_resize("255x255", path = "gay.png", directory="C:/Users/dimal/Desktop/")
-
+a = img_resize("600x800", path = "car.png", directory="C:/Users/dimal/Desktop/")
+a.show()
 # test_image = "" # file dir+name with wormat or just name with format
 # original = Image.open(test_image)
 # original.show()
