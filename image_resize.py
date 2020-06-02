31d5ed001abb023a0cb6f139d32dc05ab82636e6
ALittleMoron <dima.lunev14@gmail.com>
2020-06-02 21:56:28 +0300

    rework size getter(use default PIL .size). Add __main__ and TODO

--- image_resize.py
+++ image_resize.py
@@ -1,30 +1,33 @@
 from PIL import Image
 import math
 
+# TODO: make parsing of directory+path or directory for validation
+
 def img_resize(f_size="", path="", directory=""):
+    
     if not f_size and not path:
         raise Exception("No argument (r_size, f_size or path) in img_resize")
-    
+   
     if directory:
         with Image.open(directory+path) as img:
-            size = f_size.split('x')
-            size = tuple([int(i) for i in size])
-            img.thumbnail(size)
+            sz = img.size
+            img.load()
+            img.thumbnail(sz)
             return img
 
     elif not directory:
         with Image.open(path) as img:
-            size = f_size.split('x')
-            size = tuple([int(i) for i in size])
-            img.thumbnail(size)
+            sz = img.size
+            img.load()
+            img.thumbnail(sz)
             return img
 
     else:
         raise Exception("IDK what went wrong")
 
-
-a = img_resize("600x800", path = "car.png", directory="C:/Users/dimal/Desktop/")
-a.show()
+if __name__ == "__main__":
+    a = img_resize("500x200", path = "abs.jpg", directory="C:/python/")
+    a.show()
 # test_image = "" # file dir+name with wormat or just name with format
 # original = Image.open(test_image)
 # original.show()
