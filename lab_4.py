34ad4cb5aceb2dbdbd3e9ffa0872de25bbd6fb35
ALittleMoron <dima.lunev14@gmail.com>
2020-06-03 17:33:15 +0300

    add 2 another methods

--- lab_4.py
+++ lab_4.py
@@ -16,8 +16,49 @@ def left_point_method(func:str, diapason:list):
     summary =  0 
     for x in arange(diapason[0], diapason[1], STEP):
         summary += eval(f'math.tan({x})')
+    
     return summary * STEP
 
+
+def right_point_method(func:str, diapason:list):
+    if not func and not diapason:
+        raise Exception("no function or diapason")
+    if len(diapason) > 2:
+        raise Exception("diapason must consistrs of 2 int nums")
+
+    summary = 0
+    for x in arange(diapason[0]+STEP, diapason[1]+STEP, STEP):
+        summary += eval(f'math.tan({x})')
+    
+    return summary * STEP
+
+
+def middle_point_method(func:str, diapason:list):
+    if not func and not diapason:
+        raise Exception("no function or diapason")
+    if len(diapason) > 2:
+        raise Exception("diapason must consistrs of 2 int nums")
+
+    summary = 0
+    for x in arange(diapason[0]+STEP/2.0, diapason[1], STEP):
+        summary += eval(f'math.tan({x})')
+    
+    return summary * STEP
+
+# def trapezoid_point_method(func:str, diapason:list):
+#     if not func and not diapason:
+#         raise Exception("no function or diapason")
+#     if len(diapason) > 2:
+#         raise Exception("diapason must consistrs of 2 int nums")
+
+#     summary = 0
+#     for x in arange(diapason[0]+STEP/2.0, diapason[1], STEP):
+        
+#         summary += eval(f'math.tan({x})')
+#     return summary * STEP
+
 if __name__ == "__main__":
-    res = left_point_method(FUNC, [a,b])
-    print(res)
+    res_1 = left_point_method(FUNC, [a,b])
+    res_2 = right_point_method(FUNC, [a,b])
+    res_3 = middle_point_method(FUNC, [a,b])
+    print(res_1, res_2, res_3)
