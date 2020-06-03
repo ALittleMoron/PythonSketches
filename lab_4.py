c901062af2730b7c767af132bce44486fb0308bf
ALittleMoron <dima.lunev14@gmail.com>
2020-06-03 18:35:27 +0300

    add last 2 methods

--- lab_4.py
+++ lab_4.py
@@ -1,5 +1,6 @@
 import math
 from numpy import arange
+from timeit import timeit
 
 a = 0
 b = math.pi/4
@@ -7,6 +8,19 @@ STEP = 0.01
 FUNC = 'math.tan(x)'
 
 
+def benchmark(func):
+    import time
+    
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        return_value = func(*args, **kwargs)
+        end = time.time()
+        print(f'{func.__name__}: {end-start} секунд.')
+        return return_value
+    return wrapper
+
+
+@benchmark
 def left_point_method(func:str, diapason:list):
     if not func and not diapason:
         raise Exception("no function or diapason")
@@ -19,7 +33,7 @@ def left_point_method(func:str, diapason:list):
     
     return summary * STEP
 
-
+@benchmark
 def right_point_method(func:str, diapason:list):
     if not func and not diapason:
         raise Exception("no function or diapason")
@@ -27,12 +41,12 @@ def right_point_method(func:str, diapason:list):
         raise Exception("diapason must consistrs of 2 int nums")
 
     summary = 0
-    for x in arange(diapason[0]+STEP, diapason[1]+STEP, STEP):
+    for x in arange(diapason[0] + STEP, diapason[1] + STEP, STEP):
         summary += eval(f'math.tan({x})')
     
     return summary * STEP
 
-
+@benchmark
 def middle_point_method(func:str, diapason:list):
     if not func and not diapason:
         raise Exception("no function or diapason")
@@ -40,25 +54,60 @@ def middle_point_method(func:str, diapason:list):
         raise Exception("diapason must consistrs of 2 int nums")
 
     summary = 0
-    for x in arange(diapason[0]+STEP/2.0, diapason[1], STEP):
+    for x in arange(diapason[0] + STEP/2.0, diapason[1], STEP):
         summary += eval(f'math.tan({x})')
     
     return summary * STEP
 
-# def trapezoid_point_method(func:str, diapason:list):
-#     if not func and not diapason:
-#         raise Exception("no function or diapason")
-#     if len(diapason) > 2:
-#         raise Exception("diapason must consistrs of 2 int nums")
+@benchmark
+def trapezoid_method(func:str, diapason:list):
+    if not func and not diapason:
+        raise Exception("no function or diapason")
+    if len(diapason) > 2:
+        raise Exception("diapason must consistrs of 2 int nums")
 
-#     summary = 0
-#     for x in arange(diapason[0]+STEP/2.0, diapason[1], STEP):
-        
-#         summary += eval(f'math.tan({x})')
-#     return summary * STEP
+    summary = 0
+    for x in arange(diapason[0]+ STEP/2.0, diapason[1], STEP):
+        if (x == diapason[0]+ STEP) or (x == diapason[1]):
+            summary += eval(f'math.tan({x}) / 2')
+        else:
+            summary += eval(f'math.tan({x})')
+    
+    return summary * STEP
+
+@benchmark
+def simpsons_method(func:str, diapason:list):
+    if not func and not diapason:
+        raise Exception("no function or diapason")
+    if len(diapason) > 2:
+        raise Exception("diapason must consistrs of 2 int nums")
+
+    summary = 0
+    for i in range(100):
+        x = diapason[0] + STEP * i
+        if (i == 0) or (i == 99):
+            summary += eval(f'math.tan({x})')
+        elif (i % 2 == 1):
+            summary += eval(f'math.tan({x}) * 4')
+        elif (i % 2 == 0):
+            summary += eval(f'math.tan({x}) * 2')
+    
+    return summary * STEP/5.0
+
 
 if __name__ == "__main__":
+    print('\nВремя выполнения функций:\n')
     res_1 = left_point_method(FUNC, [a,b])
     res_2 = right_point_method(FUNC, [a,b])
     res_3 = middle_point_method(FUNC, [a,b])
-    print(res_1, res_2, res_3)
+    res_4 = trapezoid_method(FUNC, [a,b])
+    res_5 = simpsons_method(FUNC, [a,b])
+    
+    print(f'''
+\nРезультаты работы функций:
+
+res_1 = {res_1},
+res_2 = {res_2},
+res_3 = {res_3},
+res_4 = {res_4},
+res_5 = {res_5}. ''')
