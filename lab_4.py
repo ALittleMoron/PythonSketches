ec810fd71eee01cd3ae5fdbde6852b16a462b3cc
ALittleMoron <dima.lunev14@gmail.com>
2020-06-04 22:07:14 +0300

    return function argument. Applie it in eval() with .format

--- lab_4.py
+++ lab_4.py
@@ -1,11 +1,13 @@
 import math
-from numpy import arange
 import time
+
+from numpy import arange
 
 
 a = 0
 b = math.pi/4
 STEP = 0.01
+FUNC = 'math.tan({})'
 
 
 def benchmark(func):
@@ -19,68 +21,68 @@ def benchmark(func):
 
 
 @benchmark
-def left_point_method(diapason:list):
-    if not diapason:
-        raise Exception("no diapason")
+def left_point_method(func:str, diapason:list):
+    if not func and not diapason:
+        raise Exception("no function or diapason")
     if len(diapason) > 2:
         raise Exception("diapason must consistrs of 2 int nums")
 
     summary =  0 
     for x in arange(diapason[0], diapason[1], STEP):
-        summary += eval(f'math.tan({x})')
+        summary += eval(func.format(x))
     
     return summary * STEP
 
 
 @benchmark
-def right_point_method(diapason:list):
-    if not diapason:
-        raise Exception("no diapason")
+def right_point_method(func:str, diapason:list):
+    if not func and not diapason:
+        raise Exception("no function or diapason")
     if len(diapason) > 2:
         raise Exception("diapason must consistrs of 2 int nums")
 
     summary = 0
     for x in arange(diapason[0] + STEP, diapason[1] + STEP, STEP):
-        summary += eval(f'math.tan({x})')
+        summary += eval(func.format(x))
     
     return summary * STEP
 
 
 @benchmark
-def middle_point_method(diapason:list):
-    if not diapason:
-        raise Exception("no diapason")
+def middle_point_method(func:str, diapason:list):
+    if not func and not diapason:
+        raise Exception("no function or diapason")
     if len(diapason) > 2:
         raise Exception("diapason must consistrs of 2 int nums")
 
     summary = 0
     for x in arange(diapason[0] + STEP/2.0, diapason[1], STEP):
-        summary += eval(f'math.tan({x})')
+        summary += eval(func.format(x))
     
     return summary * STEP
 
 
 @benchmark
-def trapezoid_method(diapason:list):
-    if not diapason:
-        raise Exception("no diapason")
+def trapezoid_method(func:str, diapason:list):
+    if not func and not diapason:
+        raise Exception("no function or diapason")
     if len(diapason) > 2:
         raise Exception("diapason must consistrs of 2 int nums")
 
     summary = 0
     for x in arange(diapason[0]+ STEP/2.0, diapason[1], STEP):
         if (x == diapason[0]+ STEP) or (x == diapason[1]):
-            summary += eval(f'math.tan({x}) / 2')
+            summary += eval(func.format(x) + '/ 2')
         else:
-            summary += eval(f'math.tan({x})')
+            summary += eval(func.format(x))
     
     return summary * STEP
 
 
 @benchmark
-def simpsons_method(diapason:list):
-    if not diapason:
-        raise Exception("no diapason")
+def simpsons_method(func:str, diapason:list):
+    if not func and not diapason:
+        raise Exception("no function or diapason")
     if len(diapason) > 2:
         raise Exception("diapason must consistrs of 2 int nums")
 
@@ -88,28 +90,28 @@ def simpsons_method(diapason:list):
     for i in range(100):
         x = diapason[0] + STEP * i
         if (i == 0) or (i == 99):
-            summary += eval(f'math.tan({x})')
+            summary += eval(func.format(x))
         elif (i % 2 == 1):
-            summary += eval(f'math.tan({x}) * 4')
+            summary += eval(func.format(x) + '* 4')
         elif (i % 2 == 0):
-            summary += eval(f'math.tan({x}) * 2')
+            summary += eval(func.format(x) + '* 2')
     
     return summary * STEP/5.0
 
 
 if __name__ == "__main__":
     print('\nВремя выполнения функций:\n')
-    res_1 = left_point_method([a,b])
-    res_2 = right_point_method([a,b])
-    res_3 = middle_point_method([a,b])
-    res_4 = trapezoid_method([a,b])
-    res_5 = simpsons_method([a,b])
+    res_1 = left_point_method(FUNC, [a,b])
+    res_2 = right_point_method(FUNC, [a,b])
+    res_3 = middle_point_method(FUNC, [a,b])
+    res_4 = trapezoid_method(FUNC, [a,b])
+    res_5 = simpsons_method(FUNC, [a,b])
     
     print(f'''
 \nРезультаты работы функций:
 
-res_1 = {res_1},
-res_2 = {res_2},
-res_3 = {res_3},
-res_4 = {res_4},
-res_5 = {res_5}. ''')
+left_point_method = {res_1},
+right_point_method = {res_2},
+middle_point_method = {res_3},
+trapezoid_method = {res_4},
+simpsons_method = {res_5}. ''')
