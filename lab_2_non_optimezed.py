c466d77f43fc8c62457454124c5cca79672c7d15
ALittleMoron <dima.lunev14@gmail.com>
2020-06-09 18:33:18 +0300

    rework program: add function, __name__ == '__main__', and benchmark decorator

--- lab_2_non_optimezed.py
+++ lab_2_non_optimezed.py
@@ -1,33 +1,48 @@
-import time as tm
+from time import time
 import random as rd
+
 
 SIZE = 150
 
-#function that generate randome list with float nums like [0.72612,0.21261,0.16525,....,0.71126]
+
+def benchmark(func):
+    def wrapper(*args, **kwargs):
+        start = time()
+        return_value = func(*args, **kwargs)
+        end = time()
+        print(f'{func.__name__}: {end-start} секунд.')
+        return return_value
+    return wrapper
+
+
 def num_gen():
+    """function that generate randome list with float nums like [0.72612,0.21261,0.16525,....,0.71126]"""
     rnd_num = [rd.uniform(0, 1) for it in range(SIZE)]
     return rnd_num
 
-#function that generate empty list like [0,0,0,0....,0,0]
+
 def zero_gen():
+    """function that generate empty list like [0,0,0,0....,0,0]"""
     zero_list = [0 for it in range(SIZE)]
     return zero_list
 
-#main program that multiply 2 matrix size 1200x1200
-start = tm.time()
-X = [num_gen() for it in range(SIZE)]
-Y = [num_gen() for it in range(SIZE)]
-result = [zero_gen() for it in range(SIZE)]
 
-for i in range(len(X)):
-    for j in range(len(Y[0])):
-        for k in range(len(Y)):
-            result[i][j] += X[i][k] * Y[k][j]
+@benchmark
+def mitrix_multiply():
+    """main program that multiply 2 matrix size 1200x1200"""
+    X = [num_gen() for it in range(SIZE)]
+    Y = [num_gen() for it in range(SIZE)]
+    result = [zero_gen() for it in range(SIZE)]
 
-end = tm.time()
-time = (end - start)/ 1.0
-gflops_per_sec = (SIZE*SIZE*(2.0*SIZE-1.0)/1000**3)/time
+    for i in range(len(X)):
+        for j in range(len(Y[0])):
+            for k in range(len(Y)):
+                result[i][j] += X[i][k] * Y[k][j]
 
-print('\ncode runned successfully.')
-print('code runtime is ', time, 'seconds')
-print('code speed is ', gflops_per_sec, 'gigaflops per second\n')
+    gflops = (SIZE*SIZE*(2.0*SIZE-1.0)/1000**3)
+    return result, gflops
+
+
+if __name__ == "__main__":
+    a, b = mitrix_multiply()
+    print(b)
