ba484cd939213f618b0c4529ba4c408e39b58ac6
ALittleMoron <dima.lunev14@gmail.com>
2020-06-24 23:18:21 +0300

    rework code to run in console with sys.argv

--- random_password_generator.py
+++ random_password_generator.py
@@ -1,33 +1,30 @@
-from random import randint, choice
-from pyperclip import copy as pypercopy
+from random import choice
+import pyperclip
+import sys
 
 # if you need only letter+number password use selective_chars = all_chars[12:]
 all_chars = '+-/*!&$#?=@<>abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890' 
 
-def password_generator(is_all_chars:bool, length:int):
-    if not length:
-        length = 0
-    if type(is_all_chars) is not bool or type(length) is not int:
-        raise Exception("is_all_chars is not bool type or length is not int type")
-
-    if is_all_chars is True:
+if len(sys.argv) == 3: 
+    length = sys.argv[2]
+    is_all_chars = sys.argv[3]
+    if is_all_chars.lower() in ('strong', 'all') and is_all_chars.isalpha():
         password = ''
         for _ in range(length):
             password += choice(all_chars)
-    else:
+    elif is_all_chars.lower() in ('weak', 'low', 'no') and is_all_chars.isalpha():
         selective_chars = all_chars[12:]
         password = ''
         for _ in range(length):
             password += choice(selective_chars)
-    pypercopy(password)
-    return password
+    pyperclip.copy(password)
 
-if __name__ == "__main__":
-    while(True):
-        choice_length = int(input("length: "))
-        a = password_generator(False, choice_length)
-        print(a)
-        if input() != '':
-            continue
-        else:
-            break
+elif len(sys.argv) == 2:
+    selective_chars = all_chars[12:]
+    password = ''
+    for _ in range(length):
+        password += choice(selective_chars)
+    pyperclip.copy(password)
+
+print(f"Your password: {password} was copied to the clipboard")
+input("Press any key (not shutdown button) to exit... ")
