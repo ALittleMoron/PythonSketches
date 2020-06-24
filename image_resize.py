b2ba4f6ebcb0057af4a85a11e8a0f417c5a9b2ff
ALittleMoron <dima.lunev14@gmail.com>
2020-06-24 22:58:32 +0300

    add regex to path and directory

--- image_resize.py
+++ image_resize.py
@@ -1,21 +1,34 @@
 from PIL import Image
-import math
+import math, re, os, os.path
 
 # TODO: make parsing of directory+path or directory for validation
 
-def img_resize(f_size="", path="", directory=""):
+def img_resize(f_size:str="", path:str="")->Image:
+    """resize inputed img in directory and return it
     
+    key arguments:
+    f_size -- to what size you need to resize the image. Like "200x200"
+    """
     if not f_size and not path:
-        raise Exception("No argument (r_size, f_size or path) in img_resize")
-   
-    if directory:
-        with Image.open(directory+path) as img:
+        raise Exception("No argument in img_resize function")
+    if type(f_size) is not str or type(path) is not str:
+        raise Exception("function need string arguments")
+
+    full_path_pattern = re.compile(r'^([CD]:.)(\.\w{1,5})$')
+    cur_path_pattern = re.compile(r'^([a-zA-Z]+)(\.\w{1,5})$')
+
+    if full_path_pattern.fullmatch(path) is not None:
+        if not os.path.exists(path):
+            raise Exception("Path not exist")
+        with Image.open(path) as img:
             sz = img.size
             img.load()
             img.thumbnail(sz)
             return img
 
-    elif not directory:
+    elif cur_path_pattern.fullmatch(path) is not None:
+        if not os.path.isfile(path):
+            raise Exception("cringe error. how you input not file with file extention?")
         with Image.open(path) as img:
             sz = img.size
             img.load()
@@ -23,24 +36,8 @@ def img_resize(f_size="", path="", directory=""):
             return img
 
     else:
-        raise Exception("IDK what went wrong")
+        raise Exception("Not a directory or file.")
 
 if __name__ == "__main__":
-    a = img_resize("500x200", path = "abs.jpg", directory="C:/python/")
+    a = img_resize("500x200", path = "abs.jpg")
     a.show()
-# test_image = "" # file dir+name with wormat or just name with format
-# original = Image.open(test_image)
-# original.show()
-# size = 354, 472
-
-# width, height = original.size   # Get dimensions
-# left = width/4
-# top = height/8
-# right = 3 * width/4
-# bottom = height
-# cropped_example = original.crop((left, top, right, bottom))
-
-
-# cropped_example.show()
-# cropped_example.thumbnail(size)
-# cropped_example.show()
