e2674b89adb8581c30f8d5d1d7e74d7a9c5ff597
dmitriy <dima.lunev14@gmail.com>
2020-07-03 19:31:25 +0300

    Переработал код. Далее добавлю поддержку sys

--- random_password_generator.py
+++ random_password_generator.py
@@ -1,30 +1,34 @@
 from random import choice
+from string import (ascii_letters, ascii_lowercase,
+                    ascii_uppercase, digits, punctuation)
 import pyperclip
 import sys
 
-# if you need only letter+number password use selective_chars = all_chars[12:]
-all_chars = '+-/*!&$#?=@<>abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890' 
 
-if len(sys.argv) == 3: 
-    length = int(sys.argv[1])
-    is_all_chars = sys.argv[2]
-    if is_all_chars.lower() in ('strong', 'all') and is_all_chars.isalpha():
-        password = ''
-        for _ in range(length):
-            password += choice(all_chars)
-    elif is_all_chars.lower() in ('weak', 'low', 'no') and is_all_chars.isalpha():
-        selective_chars = all_chars[12:]
-        password = ''
-        for _ in range(length):
-            password += choice(selective_chars)
+# более наглядное представление возвращаемого значения
+copied_password = str
+
+
+def rnd_password_generator(length:int) -> copied_password:
+    """функция генерирует сложный случайный пароль, состоящий из
+       спец. символов, цифр, латиницы в верхнем и нижнем регистре
+
+       аргументы фунеции:
+       length -- длина генерируемого пароля
+
+       возвращаемое значение:
+       copied_password -- сгенерированный и скопированный в буфер обмена пароль
+    """
+    all_chars = ascii_letters + ascii_lowercase + ascii_uppercase +\
+                digits + punctuation
+
+    password = ''.join(choice(all_chars) for _ in range(length))
     pyperclip.copy(password)
 
-elif len(sys.argv) == 2:
-    selective_chars = all_chars[12:]
-    password = ''
-    for _ in range(length):
-        password += choice(selective_chars)
-    pyperclip.copy(password)
+    return password
 
-print(f"Your password: {password} was copied to the clipboard")
-input("Press any key (not shutdown button) to exit... ")
+
+if __name__ == "__main__":
+    a = rnd_password_generator(25)
+    print(a)
+    input("Press any key (not shutdown button) to exit... ")
