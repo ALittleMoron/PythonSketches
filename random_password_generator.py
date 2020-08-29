025c3bb5a6d3468df7ba7c310335290ad373cf79
ALittleMoron <dima.lunev14@gmail.com>
2020-08-29 21:27:18 +0300

    наконец-то добавил несколько вариантов генерации + их выбор по sys.argv

--- random_password_generator.py
+++ random_password_generator.py
@@ -9,26 +9,40 @@ import sys
 copied_password = str
 
 
-def rnd_password_generator(length:int) -> copied_password:
+def rnd_password_generator(length:int, char_set:str) -> copied_password:
     """функция генерирует сложный случайный пароль, состоящий из
        спец. символов, цифр, латиницы в верхнем и нижнем регистре
 
        аргументы фунеции:
        length -- длина генерируемого пароля
+       char_set -- набор символов в виде строки
 
        возвращаемое значение:
        copied_password -- сгенерированный и скопированный в буфер обмена пароль
     """
-    all_chars = ascii_letters + ascii_lowercase + ascii_uppercase +\
-                digits + punctuation
-
-    password = ''.join(choice(all_chars) for _ in range(length))
+    password = ''.join(choice(char_set) for _ in range(length))
     pyperclip.copy(password)
 
     return password
 
 
 if __name__ == "__main__":
-    a = rnd_password_generator(25)
-    print(a)
+    if len(sys.argv) > 1:
+        if len(sys.argv) == 2:
+            chars = ascii_letters + ascii_lowercase + ascii_uppercase +\
+                    digits
+        elif len(sys.argv) == 3 and sys.argv[2] in ('1', 'True', 'yes', 'y',
+                                                    'Y'):
+            chars = ascii_letters + ascii_lowercase + ascii_uppercase +\
+                    digits + punctuation
+        elif len(sys.argv) == 3 and sys.argv[2] in ('0', 'False', 'no', 'n',
+                                                    'N'):
+            chars = ascii_letters + ascii_lowercase + ascii_uppercase +\
+                    digits
+        else:
+            raise Exception('oops!')
+        print(rnd_password_generator(int(sys.argv[1]), chars))
+
+    else:
+        print('use arguments to execute code (args: length, char_set)')
     input("Press any key (not shutdown button) to exit... ")
